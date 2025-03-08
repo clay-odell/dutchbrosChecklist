a79f622844fe41c24c0007951ba000697e9326e7
Clay O'Dell <clayodell.swe@gmail.com>
2025-03-08 10:51:05 -0600

    aligned the checklists

--- src/TaskSection.jsx
+++ src/TaskSection.jsx
@@ -9,24 +9,29 @@ const TaskSection = ({
   onToggleTask,
 }) => (
   <section className="check-containers">
-    <h3>
+    <h3 className="text-start">
+      {/* Align Form.Check label and switch to the left */}
       <Form.Check
         reverse
         type="switch"
         label={title}
         checked={allChecked}
         onChange={onToggleAll}
+        className="text-start"
       />
     </h3>
     <ul>
       {tasks.map((task, index) => (
-        <li key={index}>
+        <li key={index} className="text-start">
+          {" "}
+          {/* Add alignment to the list items */}
           <Form.Check
             reverse
             type="switch"
             label={task}
             checked={taskStates[index]}
             onChange={() => onToggleTask(index)}
+            className="text-start"
           />
         </li>
       ))}
